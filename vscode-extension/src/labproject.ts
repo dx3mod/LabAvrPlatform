89d7fd59ec3b0fcfde50af84bec07abaee0d5bfe
dx3mod <dx3mod@bk.ru>
2024-04-01 22:07:21 +0300

    Add basic LabAvrProjectConfig

---
+++ vscode-extension/src/labproject.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+export const LabAvrProjectConfigScheme = z.object({
+  artifactName: z.string(),
+
+  target: z.object({
+    mcu: z.string(),
+    hz: z.number(),
+  }),
+
+  layout: z.object({
+    sourcesDir: z.string(),
+    headersDir: z.string(),
+    vendorDir: z.string(),
+    buildDir: z.string(),
+  }),
+});
+
+export type LabAvrProjectConfig = z.infer<typeof LabAvrProjectConfigScheme>;
+
+export function createLabAvrProjectConfig(
+  config: Pick<LabAvrProjectConfig, "artifactName" | "target">,
+): LabAvrProjectConfig {
+  return {
+    artifactName: config.artifactName,
+    target: config.target,
+    layout: {
+      sourcesDir: "src",
+      headersDir: "include",
+      buildDir: "_build",
+      vendorDir: "vendor",
+    },
+  };
+}
