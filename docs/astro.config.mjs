8fa4f012110749a29abfd71f22a8a0055c9bda06
dx3mod <dx3mod@bk.ru>
2024-05-08 23:49:36 +0300

    docs: basic

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,23 +5,41 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'labavrplatform',
+			locales: {
+				root: {
+					label: 'Русский',
+					lang: 'ru',
+				},
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/dx3mod/LabAvrPlatform',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Первые шаги',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
+						{ label: 'Установка', link: "/guide/installation" },
+						{ label: 'Первый проект', link: "/guide/first-project" },
+						{ label: 'Сторонние библиотеки', link: "/guide/import-library" },
+					]
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', link: '/guides/example/' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
 			],
+			customCss: [
+				"./src/styles/custom.css"
+			],
+			
 		}),
 	],
 });
---
+++ docs/src/content/docs/guide/first-project.md
@@ -0,0 +1,4 @@
+---
+title: Первый проект
+# description: A reference page in my new Starlight docs site.
+---
