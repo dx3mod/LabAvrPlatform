cc0377b2b4327a851b1185018a7c5e8619b1c7ee
dx3mod <dx3mod@bk.ru>
2024-06-03 12:57:55 +0300

    feat(doc): add description to README

--- www/src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'LabAvrPlatform';
-export const SITE_DESCRIPTION = 'Lab-based development platform for AVR C/C++ projects.';
-
-// Remember to set your site URL in astro.config.mjs
